ef5fa409d717807875f7a1cccae888eb834841d6
Robert Hodges <nexus000110@gmail.com>
2020-12-17 10:59:06 -0600

    Single Page Applications Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +11,15 @@ export class DishService {
 
   getDishes(): Dish[] {
     return DISHES;
+  }
+
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => 
+    (dish.id === id))[0];
+  } 
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => 
+    dish.featured)[0];
   }
 }
