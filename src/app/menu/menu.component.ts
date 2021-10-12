8d83eb5f9253f20bea0a98db85451271ba592783
Robert Hodges <nexus000110@gmail.com>
2021-10-12 14:02:45 -0500

    Animations Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 
 export class MenuComponent implements OnInit {
@@ -15,10 +24,10 @@ export class MenuComponent implements OnInit {
 
   constructor(private dishService: DishService,
     @Inject('baseURL') private baseURL) { }
-  
+
   ngOnInit() {
     this.dishService.getDishes()
       .subscribe((dishes) => this.dishes = dishes,
-      errmess => this.errMess = <any>errmess);
+        errmess => this.errMess = <any>errmess);
   }
 }
